6577b3ab71d8fd7372b662244eb7fa488755d957
eman1970 <tommy.hallqvist@gmail.com>
2021-03-16 21:24:07 +0100

    Connect to Heroku Backend

--- src/shared/api/BackendAPI.ts
+++ src/shared/api/BackendAPI.ts
@@ -1,7 +1,11 @@
 import Axios from 'axios'
 
+const development = 'http://localhost:3002'
+const production = 'https://tommytestapp-2021.herokuapp.com/'
+
 const BackendAPI = Axios.create({
-    baseURL:'http://localhost:3002'
+    baseURL: production,
+    headers: {'Content-type': 'application/json'}
 })
 
 export default BackendAPI
